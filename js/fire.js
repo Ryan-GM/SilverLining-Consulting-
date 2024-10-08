77999b66ed482edc7da7e26924444e4a1a5d48ca
Ryan-GM <71759184+Ryan-GM@users.noreply.github.com>
2024-10-08 08:50:10 +0300

    Show the current tab and add active class to the clicked tab

--- js/fire.js
+++ js/fire.js
@@ -17,4 +17,24 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  function openTab(evt, tabName) {
+    // Hide all tab content
+    var i, tabContent, tabButtons;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+  
+    // Remove active class from all tab buttons
+    tabButtons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab and add active class to the clicked tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
   
